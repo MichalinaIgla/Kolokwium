141ffd39d123b1c1ba63f5e483f95da8d80d6b4e
MichalinaIgla <michalina@vps714325.ovh.net>
2020-03-04 11:37:08 +0100

    commit

---
+++ zad6/build/CMakeFiles/Student.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student.dir/src/address.cpp.o"
+  "CMakeFiles/Student.dir/src/employee.cpp.o"
+  "CMakeFiles/Student.dir/src/main.cpp.o"
+  "libStudent.pdb"
+  "libStudent.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
